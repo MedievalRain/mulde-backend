3d5e39e3268eb1ed0a9a6b0692cad27ee017ea12
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-12-06 21:10:08 +0300

    Rename method

--- src/api/routes/userRouter.ts
+++ src/api/routes/userRouter.ts
@@ -5,7 +5,7 @@ const userRouter = express.Router();
 
 userRouter.post("/register", async (req, res, next) => {
   try {
-    await userService.createUser(req.body);
+    await userService.register(req.body);
     res.status(200).json({
       message: "User registered",
     });
--- src/domain/user/userService.ts
+++ src/domain/user/userService.ts
@@ -8,7 +8,7 @@ import * as bcrypt from "bcrypt";
 class UserService {
   constructor(private userRepository: UserRepository) {}
 
-  public async createUser(data: unknown) {
+  public async register(data: unknown) {
     const { email, password } = parseAuthRequest(data);
     const passwordHash = await bcrypt.hash(password, 10);
     return this.userRepository.createUser(email, passwordHash, "user");

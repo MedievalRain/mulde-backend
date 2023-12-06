fe33eab31e2c33ce04c8a5b8aafbd93757574a5e
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-12-07 01:05:24 +0300

    Formatting

--- src/domain/user/userService.ts
+++ src/domain/user/userService.ts
@@ -11,37 +11,18 @@ class UserService {
   public async register(data: unknown) {
     const { email, password, inviteId } = parseRegisterRequest(data);
     const passwordHash = await bcrypt.hash(password, 10);
-    return this.userRepository.createUser(
-      email,
-      passwordHash,
-      "user",
-      inviteId
-    );
+    return this.userRepository.createUser(email, passwordHash, "user", inviteId);
   }
 
   public async login(data: unknown) {
     const { email, password } = parseAuthRequest(data);
-    const {
-      id: userId,
-      passwordHash,
-      role,
-    } = await this.userRepository.getUserByEmail(email);
+    const { id: userId, passwordHash, role } = await this.userRepository.getUserByEmail(email);
     const isPasswordMatch = await bcrypt.compare(password, passwordHash);
     if (isPasswordMatch) {
-      const sessionExpireDate = new Date(
-        new Date().getTime() + 30 * 24 * 60 * 60000
-      ); // 30 days
+      const sessionExpireDate = new Date(new Date().getTime() + 30 * 24 * 60 * 60000); // 30 days
       const sessionId = randomUUID();
-      await this.userRepository.createSession(
-        sessionId,
-        userId,
-        sessionExpireDate
-      );
-      const refreshToken = jwtService.generateRefreshToken(
-        sessionId,
-        userId,
-        role
-      );
+      await this.userRepository.createSession(sessionId, userId, sessionExpireDate);
+      const refreshToken = jwtService.generateRefreshToken(sessionId, userId, role);
       const accessToken = jwtService.generateAccessToken(userId, role);
       return { accessToken, refreshToken, sessionExpireDate };
     } else {

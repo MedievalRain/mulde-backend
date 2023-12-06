070d8a7e8a166890a1903dfcaa43d30dbd082a10
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-12-07 01:42:19 +0300

    Disable notice logs

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,4 +2,4 @@ import postgres from "postgres";
 import { EnvError } from "../errors/EnvError";
 const DATABASE_URL = process.env.DATABASE_URL;
 if (!DATABASE_URL) throw new EnvError("FRONTEND_DOMAIN");
-export const sql = postgres(DATABASE_URL, { transform: postgres.camel });
+export const sql = postgres(DATABASE_URL, { transform: postgres.camel, onnotice: () => {} });
